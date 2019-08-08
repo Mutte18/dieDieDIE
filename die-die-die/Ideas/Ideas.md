56c043930433f6e69ba5a56a741ca9b85caf492b
Mattias <mattias.vidhall@hotmail.com>
2019-08-08 22:12:42 +0200

    Added drawings of how project should function

---
+++ die-die-die/Ideas/Ideas.md
@@ -0,0 +1,4 @@
+Components:
+  * Die - Prop stored as number
+  
+  
---
+++ die-die-die/src/components/Die.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Die',
+};
+</script>
+
+<style scoped>
+
+</style>
